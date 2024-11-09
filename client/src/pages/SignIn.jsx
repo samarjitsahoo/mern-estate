caafc50342d31e0180c8ae9787ce9921fff5c132
Samarjit Sahoo <samarjit9203@gmail.com>
2024-11-09 06:43:41 +0530

    Create and add OAuth component

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,6 +6,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -68,10 +69,11 @@ export default function SignIn() {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Don not have an account?</p>
-        <Link to={"/sign-in"}>
+        <Link to={"/sign-up"}>
           <span className="text-blue-700">Sign Up</span>
         </Link>
       </div>
